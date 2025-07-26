7dd61f364990f55fe0c80bedd961cd895234d56d
AllensTechChannel <allenstechchannel@gmail.com>
2025-07-26 00:33:02 -0400

    Delete PythonOS directory

--- PythonOS/README.txt
+++
@@ -1,3 +0,0 @@
-When Running Python OS for the first time run setup.bat.
-If Python OS is not ran through setup.bat on first run several modules will be missing.
-   
--- PythonOS/screensaver/mystify/Install-pygame.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd dvd
-py -m pip install pygame
-C:\Users\%username%\AppData\Local\Programs\Python\Python313\python.exe -m pip install --upgrade pip
-
-mystify.py
