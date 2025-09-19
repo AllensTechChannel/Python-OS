b55db6e9a7a2e6dd32fd5830a2094d22d37c3295
AllensTechChannel <allenstechchannel@gmail.com>
2025-09-19 16:26:12 -0400

    Update setup.bat

--- PythonOS/setup.bat
+++ PythonOS/setup.bat
@@ -5,8 +5,17 @@ start "" python-install.bat
 
 pause
 
-py -m  pip install Pillow ttkbootstrap screen-brightness-control keyboard psutil playsound Webview
+py -m  pip install Pillow 
+py -m  pip install ttkbootstrap 
+py -m  pip install screen-brightness-control 
+py -m  pip install keyboard 
+py -m  pip install psutil 
+
+py -m  pip install Webview
 C:\Users\%username%\AppData\Local\Programs\Python\Python313\python.exe -m pip install --upgrade pip
+
+py -m pip install playsound==1.2.2
 cls
 boot.py
+
 pause
